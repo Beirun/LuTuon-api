9278e330de5dc7e144f315f662b5e1d98fa8ce1e
Beirun <orillobernardjay@gmail.com>
2025-07-21 01:16:21 +0800

    Add user schema

---
+++ src/schema/user.ts
@@ -0,0 +1,15 @@
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { role } from './role';
+import { avatar } from './avatar';
+
+export const user = pgTable('user', {
+  userId: uuid('user_id').primaryKey(),
+  roleId: uuid('role_id').references(() => role.roleId).notNull(),
+  userEmail: text('user_email').unique().notNull(),
+  userName: text('user_name').unique().notNull(),
+  passwordHash: text('password_hash').notNull(),
+  userDob: timestamp('user_dob', { withTimezone: true }).notNull(),
+  dateCreated: timestamp('date_created', { withTimezone: true }).notNull(),
+  dateUpdated: timestamp('date_updated', { withTimezone: true }).notNull(),
+  avatarId: uuid('avatar_id').references(() => avatar.avatarId).notNull()
+});
