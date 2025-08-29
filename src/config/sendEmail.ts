25a84156aee8272c3c7deaf7654d641322b9629c
Beirun <orillobernardjay@gmail.com>
2025-08-29 23:05:44 +0800

    add: sendEmail config

---
+++ src/config/sendEmail.ts
@@ -0,0 +1,27 @@
+// config/sendEmail.ts
+import nodemailer from "nodemailer";
+
+export async function sendEmail(to: string, subject: string, html: string) {
+  if (!process.env.EMAIL || !process.env.PASSWORD) {
+    throw new Error("EMAIL and PASSWORD must be set in environment");
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: `"LuTuon Support" <${process.env.EMAIL}>`,
+      to,
+      subject,
+      html,
+    });
+  } catch (e) {
+    throw new Error("Failed to send email: " + (e as Error).message);
+  }
+}
