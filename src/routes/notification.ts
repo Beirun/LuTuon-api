fd573ed12fd4b02b2d288a417ee25f0b1ec11524
Beirun <orillobernardjay@gmail.com>
2025-08-31 22:24:00 +0800

    feat: update notification route

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { NotificationController } from "../controllers/notificationController";
+import { authenticateToken } from "../middlewares/auth";
 
 const router = Router();
 
-router.post("/", NotificationController.createNotification);
-router.get("/:userId", NotificationController.getNotificationsByUser);
-router.put("/:notificationId", NotificationController.updateNotificationStatus);
-router.delete("/:notificationId", NotificationController.deleteNotification);
+router.post("/", authenticateToken, NotificationController.createNotification);
+router.get("/:userId", authenticateToken, NotificationController.getNotificationsByUser);
+router.put("/:notificationId", authenticateToken, NotificationController.updateNotificationStatus);
+router.delete("/:notificationId", authenticateToken, NotificationController.deleteNotification);
 
 export default router;
