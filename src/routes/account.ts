5fec4f68c74f75bf86ff0824729c72c36f310beb
Beirun <orillobernardjay@gmail.com>
2025-09-09 18:14:43 +0800

    chore: update account routes

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,5 +1,5 @@
 // routes/account.ts
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { AccountController } from "../controllers/accountController";
 import { authenticateToken } from "../middlewares/auth";
 
@@ -12,7 +12,7 @@ router.post("/logout", AccountController.logout);
 router.post("/google", AccountController.google);
 
 
-router.get("/token/verify", authenticateToken, async (req, res) => res.sendStatus(200));
+router.get("/token/verify", authenticateToken, async (req, res) => res.json());
 router.get("/me", authenticateToken, AccountController.getMe);
 router.get("/users", authenticateToken, AccountController.getAllUsers);
 router.get("/users/:userId", authenticateToken, AccountController.getUserById);
