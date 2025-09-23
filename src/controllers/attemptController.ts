9c8a73f2ae0658d4de6ee52d62e5f444178399f0
Beirun <orillobernardjay@gmail.com>
2025-09-23 16:03:41 +0800

    Update attempt controller

--- src/controllers/attemptController.ts
+++ src/controllers/attemptController.ts
@@ -1,6 +1,7 @@
 // controllers/attemptController.ts
 import { Request, Response } from "express";
 import { AttemptService } from "../services/attemptService";
+import { AuthRequest } from "middlewares/auth";
 
 const attemptService = new AttemptService();
 
@@ -13,26 +14,25 @@ export class AttemptController {
       res.status(500).json({ error: (e as Error).message });
     }
   }
-  static async getAttemptByUserId(req: Request, res: Response) {
-    const {userId} = req.params;
+  static async getAttemptByUserId(req: AuthRequest, res: Response) {
     try {
-      const data = await attemptService.getAllAttemptByUserId(userId);
+      const data = await attemptService.getAllAttemptByUserId(req.user.userId);
       res.status(200).json(data);
     } catch (e) {
       res.status(500).json({ error: (e as Error).message });
     }
   }
 
-  static async postAttempt(req: Request, res: Response) {
+  static async postAttempt(req: AuthRequest, res: Response) {
     try {
-      const { userId, foodId, attemptPoint, attemptDate, attemptDuration, attemptType } = req.body;
+      const { foodId, attemptPoint, attemptDate, attemptDuration, attemptType } = req.body;
 
-      if (!userId || !foodId || !attemptPoint || !attemptDate || !attemptDuration || !attemptType) {
+      if (!foodId || !attemptPoint || !attemptDate || !attemptDuration || !attemptType) {
         return res.status(400).json({ error: "Missing required fields" });
       }
 
       const data = await attemptService.createAttempt({
-        userId,
+        userId: req.user.userId,
         foodId,
         attemptPoint: Number(attemptPoint),
         attemptDate: new Date(attemptDate),
