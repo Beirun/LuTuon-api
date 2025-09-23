93e941fbf34cfe8833a3f96e0ed26ebd44b561f9
Beirun <orillobernardjay@gmail.com>
2025-09-23 16:03:41 +0800

    Update notification controller

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import { NotificationService } from "../services/notificationService";
+import { AuthRequest } from "middlewares/auth";
 
 const notificationService = new NotificationService();
 
 export class NotificationController {
   // Create
-  static async createNotification(req: Request, res: Response) {
+  static async createNotification(req: AuthRequest, res: Response) {
     try {
-      const newNotification = await notificationService.createNotification(req.body);
+      const newNotification = await notificationService.createNotification(req.user.userId,req.body);
       res.status(201).json(newNotification);
     } catch (error: any) {
       res.status(500).json({ message: error.message });
@@ -15,10 +16,9 @@ export class NotificationController {
   }
 
   // Get all by user
-  static async getNotificationsByUser(req: Request, res: Response) {
+  static async getNotificationsByUser(req: AuthRequest, res: Response) {
     try {
-      const { userId } = req.params;
-      const notifications = await notificationService.getNotificationsByUser(userId);
+      const notifications = await notificationService.getNotificationsByUser(req.user.userId);
       res.json(notifications);
     } catch (error: any) {
       res.status(500).json({ message: error.message });
